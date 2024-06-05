0ab4bd39aaae07b4f744246675eb890ecac2d239
Shu <40323042+luo-s@users.noreply.github.com>
2024-06-05 13:50:16 -0400

    wip

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 let num1 = document.getElementById("number1");
 let num2 = document.getElementById("number2");
 let operator = document.getElementById("operator");
+let streak = document.getElementById("streak");
+let score = document.getElementById("score");
+
+let inputElement = document.getElementById("input");
+let answer = document.getElementById("check");
+answer.addEventListener("click", compare);
 
 let n1, n2, op, result;
+let streakCount = 0;
+let scoreCount = 0;
 
 function getRandomQuiz() {
   n1 = Math.floor(Math.random() * (999 - 100) + 100);
@@ -13,20 +21,23 @@ function getRandomQuiz() {
   num1.innerText = n1;
   num2.innerText = `${n2} = `;
   operator.innerText = op;
+  streak.innerText = streakCount;
+  score.innerText = scoreCount;
 }
-let inputElement = document.getElementById("input");
-let answer = document.getElementById("check");
-answer.addEventListener("click", compare);
 
 function compare(event) {
   event.preventDefault();
   let input = document.getElementById("input").value;
   console.log(input);
   if (input == result) {
-    console.log("Correct!");
+    scoreCount += 2;
+    streakCount += 1;
     getRandomQuiz();
   } else {
-    console.log("Incorrect!");
+    scoreCount -= 1;
+    streakCount = 0;
+    streak.innerText = streakCount;
+    score.innerText = scoreCount;
   }
   inputElement.value = "";
 }
